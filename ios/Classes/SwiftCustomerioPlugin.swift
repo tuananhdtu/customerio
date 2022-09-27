f76a6092c6bec6cb93031bdcd74406bbc38e81c5
tuananhdtu <tuananhdtu@gmail.com>
2022-09-27 20:28:24 +0700

    Update SwiftCustomerioPlugin.swift

--- ios/Classes/SwiftCustomerioPlugin.swift
+++ ios/Classes/SwiftCustomerioPlugin.swift
@@ -24,6 +24,7 @@ public class SwiftCustomerioPlugin: NSObject, FlutterPlugin, UIApplicationDelega
 
           _ =  CustomerIO(siteId: siteId as! String, apiKey: apiKey as! String, region: Region.EU)
           
+          UNUserNotificationCenter.current().delegate = self
           UIApplication.shared.registerForRemoteNotifications()
           result("OK")
           break;
@@ -44,5 +45,55 @@ public class SwiftCustomerioPlugin: NSObject, FlutterPlugin, UIApplicationDelega
     public  func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
         MessagingPush.shared.application(application, didFailToRegisterForRemoteNotificationsWithError: error)
     }
-}
+    
+    public func application(_ application: UIApplication, didReceiveRemoteNotification data: [AnyHashable : Any]) {
+        // Print notification payload data
+        print("Push notification received: \(data)")
 
+        // Customer.io push notifications include data regarding the push
+        // message in the data part of the payload which can be used to send
+        // feedback into our system.
+        var deliveryID: String = "";
+        if let value = data["CIO-Delivery-ID"] {
+            deliveryID = String(describing: value)
+        }
+        var deliveryToken: String = "";
+        if let value = data["CIO-Delivery-Token"] {
+            deliveryToken = String(describing: value)
+        }
+        MessagingPush.shared.trackMetric(deliveryID: deliveryID, event: .delivered, deviceToken: deliveryToken)
+
+    }
+}
+    
+
+extension SwiftCustomerioPlugin: UNUserNotificationCenterDelegate {
+    public func userNotificationCenter(
+     _ center: UNUserNotificationCenter,
+     didReceive response: UNNotificationResponse,
+     withCompletionHandler completionHandler: @escaping () -> Void
+ ) {
+     let handled = MessagingPush.shared.userNotificationCenter(center, didReceive: response,
+                                                              withCompletionHandler: completionHandler)
+     // If the Customer.io SDK does not handle the push, it's up to you to handle it and call the
+     // completion handler. If the SDK did handle it, it called the completion handler for you.
+     if !handled {
+         completionHandler()
+     }
+ }
+    
+    // OPTIONAL: If you want your push UI to show even with the app in the foreground, override this function and call
+    // the completion handler.
+    @available(iOS 10.0, *)
+    public func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        if #available(iOS 14.0, *) {
+            completionHandler([.list, .banner, .badge, .sound])
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+}
